f187db0fbe23386964d119fbbc0e8012db981afa
Alex Pritchard <hi@apritc.com>
2021-12-21 18:55:59 +0000

    Added main working scene

--- friendshaped/Assets/Scripts/Player/PlayerManager.cs
+++ friendshaped/Assets/Scripts/Player/PlayerManager.cs
@@ -8,7 +8,7 @@ namespace Player {
         [SerializeField] private MovementStates playerState;
         private PlayerControls controls;
         private float speed;
-        [SerializeField] [Range(1f,2.5f)] private float speedModifier;
+        [SerializeField] [Range(1f,10)] private float speedModifier;
 
         private bool isMovingLeft;
         private bool isMovingRight;
@@ -17,8 +17,12 @@ namespace Player {
 
         public MovementStates PlayerState { get => playerState; set => playerState = value;}
 
+        private Rigidbody2D rb2D;
+        private Vector2 velocity;
+
         // Start is called before the first frame update
         void Awake() {
+            rb2D = GetComponent<Rigidbody2D>();
             controls = new PlayerControls();
 
             // Input callbacks
@@ -31,28 +35,24 @@ namespace Player {
             controls.inventory.pickUpItem.performed += ctx => PickUpItem();
         }
 
-        private void Update() {
-
-            switch (isMovingLeft, isMovingRight) {
-                case (true,false):
-                    playerState = MovementStates.WALKING; 
-                    speed = -0.01f * speedModifier;
+        private void FixedUpdate() {
+            switch (isMovingLeft, isMovingRight)
+            {
+                case (true, false):
+                    playerState = MovementStates.WALKING;
+                    velocity = new Vector2(-1f * speedModifier, 0f);
                     break;
-                case (false,true):
-                    playerState = MovementStates.WALKING; 
-                    speed = 0.01f * speedModifier;
+                case (false, true):
+                    playerState = MovementStates.WALKING;
+                    velocity = new Vector2(speedModifier, 0f);
                     break;
                 default:
-                    playerState = MovementStates.NONE; 
-                    speed = 0f * speedModifier;
+                    playerState = MovementStates.NONE;
+                    velocity = new Vector2(0f, 0f);
                     break;
             }
-            
-            Move(speed);
-        }
- 
-        void Move(float x) {
-            transform.position += new Vector3(x, 0f, 0f);
+
+            rb2D.MovePosition(rb2D.position + velocity * Time.fixedDeltaTime);
         }
 
         void PickUpItem() {
