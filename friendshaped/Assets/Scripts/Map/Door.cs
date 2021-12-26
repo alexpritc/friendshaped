65bb6f0f17e5039136f1011e5529c277d5019591
Alex Pritchard <hi@apritc.com>
2021-12-26 17:39:13 +0000

    Added movement between carridges

--- friendshaped/Assets/Scripts/Map/Door.cs
+++ friendshaped/Assets/Scripts/Map/Door.cs
@@ -2,17 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour
+namespace Map
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Door : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Room roomOne;
+        [SerializeField] private Room roomTwo;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // TODO: Change to OnTriggerStay2D
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            // TODO: Add user interaction rather than
+            if (collision.tag == "Player"){
+                ToggleFog(roomOne, roomTwo);
+                MovePlayer(collision, roomTwo);
+            }
+        }
+
+        // Changes which room is visible
+        private void ToggleFog(Room roomA, Room roomB)
+        {
+
+            if (roomA.IsRevealed && !roomB.IsRevealed)
+            {
+
+                roomB.RevealRoom();
+                roomA.HideRoom();
+            }
+            else
+            {
+                roomA.RevealRoom();
+                roomB.HideRoom();
+            }
+        }
+
+        // Move Player
+        private void MovePlayer(Collider2D collision, Room roomB)
+        {
+            Vector2 newPos;
+
+            if (gameObject.name.Contains("Left"))
+            {
+                newPos = new Vector2(roomB.rightDoor.position.x, collision.gameObject.transform.position.y);
+            }
+            else
+            {
+                newPos = new Vector2(roomB.leftDoor.position.x, collision.gameObject.transform.position.y);
+            }
+
+            collision.transform.position = newPos;
+            Camera.main.transform.position = new Vector3(collision.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+        }
     }
 }
--- friendshaped/Assets/Scripts/Map/Room.cs
+++ friendshaped/Assets/Scripts/Map/Room.cs
@@ -4,15 +4,18 @@ namespace Map {
     public class Room : MonoBehaviour {
         [SerializeField] private bool isRevealed;
         [SerializeField] private GameObject outerCar;
+
+        public Transform leftDoor;
+        public Transform rightDoor;
         public bool IsRevealed { get => isRevealed; set => isRevealed = value; }
 
         public void RevealRoom() {
-            outerCar.SetActive(true);
+            outerCar.SetActive(false);
             IsRevealed = true;
         }
         
         public void HideRoom() {
-            outerCar.SetActive(false);
+            outerCar.SetActive(true);
             IsRevealed = false;
         }
     }
