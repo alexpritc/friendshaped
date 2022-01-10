1a977e5cf6352299ebddc13080560dbd70d530c2
Alex Pritchard <hi@apritc.com>
2022-01-10 12:53:36 +0000

    Removed unused variables

--- friendshaped/Assets/Scripts/NPC.cs
+++ friendshaped/Assets/Scripts/NPC.cs
@@ -9,19 +9,10 @@ public class NPC : MonoBehaviour
     public Sprite chatWindowSprite;
     public Sprite chatWindowBackground;
     
-    public TextAsset script;
     [Header("Ink Knots")]
     public string myIntro;
-
-    //_inkStory.ChoosePathString("LadyIntro");
-    //_inkStory.ChoosePathString("myKnotName");
     
     [Header("In-Game")]
     public Sprite mainSprite;
     public Sprite interactionSprite;
-
-    private void Start()
-    {
-        Resources.Load<TextAsset>("Ink Scripts/AllAboard.json");
-    }
 }
