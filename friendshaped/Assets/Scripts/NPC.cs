0c682f5cd0b322c8910f098b3dbdeef0cfbb4b9b
JosieGameDev <43572951+josie-wood@users.noreply.github.com>
2022-01-10 16:24:19 +0000

    Juice - npc outline
    NPC outlines change from white to yellow when you can interact with them

--- friendshaped/Assets/Scripts/NPC.cs
+++ friendshaped/Assets/Scripts/NPC.cs
@@ -11,8 +11,19 @@ public class NPC : MonoBehaviour
     public TextAsset script;
     
     [Header("In-Game")]
-    public Sprite mainSprite;
-    public Sprite interactionSprite;
-    
-    
+    public SpriteRenderer outlineSprite;
+    public Color highlightedColour = new Color(0.86f,0.65f,0.19f,1f);
+
+    private void Start()
+    {
+        outlineSprite = this.gameObject.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        outlineSprite.color = highlightedColour;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        outlineSprite.color = Color.white;
+    }
 }
